4bf0c7298aa62e3dd0ef207be8662c120451c29f
timlohrer <me@timlohrer.de>
2023-07-05 16:21:59 +0000

    add 200 response on /api for uptime tracker

--- src/routes/api/apiRouter.js
+++ src/routes/api/apiRouter.js
@@ -3,4 +3,6 @@ const apiRouter = Router();
 
 apiRouter.use('/reminders', require('./reminders/remindersRouter'));
 
+apiRouter.get('/', (req, res) => res.sendStatus(200))
+
 module.exports = apiRouter;
